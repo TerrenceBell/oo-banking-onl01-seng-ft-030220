420583001fc8c8cc9dd68632f1a73aa57bd6bc1d
Terrence Bell <terrencekbell@gmail.com>
2020-03-19 19:14:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,4 +25,11 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
+  def reverse_transfer 
+    if self.status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount 
+      @status = "reversed"
+    end 
+  
 end
